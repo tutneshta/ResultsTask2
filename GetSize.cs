9b8997ab3ad97db9c8edeaae225c40e463ac6cf8
tutneshta <tutneshta@yandex.ru>
2022-09-06 09:58:56 +0300

    Add project files.

---
+++ GetSize.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ResultsTask2
+{
+    class GetSize
+    {
+        static long count = 0;
+        public static long GetSizeDir(string path)
+        {
+
+            DirectoryInfo fileAndDirectory = new DirectoryInfo(path);
+
+
+            if (!fileAndDirectory.Exists)
+            {
+                Console.WriteLine("папка по заданному адресу не существует");
+                return 0;
+            }
+            else
+            {
+                try
+                {
+                    foreach (FileInfo f in fileAndDirectory.GetFiles())
+                    {
+
+                        GetSize.count = (long)(count + f.Length);
+                    }
+
+                    foreach (DirectoryInfo d in fileAndDirectory.GetDirectories())
+                    {
+                        GetSizeDir(d.FullName);
+                    }
+                }
+
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Ошибка: ", ex.Message);
+                }
+            }
+
+            return count;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ResultsTask2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\in\";
+            
+            Console.WriteLine($"Размер папки: {GetSize.GetSizeDir(path)} байт");
+        }
+    }
+}
